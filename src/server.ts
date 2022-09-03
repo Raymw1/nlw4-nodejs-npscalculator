a3f39402dede21cab4e156c405ffa8afe149c830
Rayan Wilbert <rayan.rmw@gmail.com>
2022-09-02 23:05:06 -0300

    Users entity/model created

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,10 @@
 import 'reflect-metadata';
-import './database';
 import express from 'express';
+import './database';
+import { routes } from './routes';
 
 const app = express();
-
-app.get('/', (request, response) =>
-  response.json({ message: 'Hello, World!' })
-);
-
-app.post('/', (request, response) => {
-  return response.json({ message: 'Data received!' });
-});
+app.use(express.json());
+app.use(routes);
 
 app.listen(3333, () => console.log('Go to http://localhost:3333'));
