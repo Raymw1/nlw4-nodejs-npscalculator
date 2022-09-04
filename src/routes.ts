097fc432e7fd2b0a5fa098c3497d232c904f9c56
Rayan Wilbert <rayan.rmw@gmail.com>
2022-09-03 22:01:53 -0300

    Surveys Model and create survey function done

--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,12 @@
 import { Router } from 'express';
+import { SurveysController } from './controllers/SurveysController';
 import { UserController } from './controllers/UserController';
 const routes = Router();
 
 const userController = new UserController();
+const surveyController = new SurveysController();
 
 routes.post('/users', userController.create);
+routes.post('/surveys', surveyController.create);
 
 export { routes };
