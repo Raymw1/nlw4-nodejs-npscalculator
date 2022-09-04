34c0cb705e1fd5692c2830769fdc771cf4ea8fff
Rayan Wilbert <rayan.rmw@gmail.com>
2022-09-04 20:45:49 -0300

    SurveysUsers model created

--- src/routes.ts
+++ src/routes.ts
@@ -1,13 +1,16 @@
 import { Router } from 'express';
+import { SendMailController } from './controllers/SendMailController';
 import { SurveysController } from './controllers/SurveysController';
 import { UserController } from './controllers/UserController';
 const routes = Router();
 
 const userController = new UserController();
 const surveyController = new SurveysController();
+const sendMailController = new SendMailController();
 
 routes.post('/users', userController.create);
 routes.get('/surveys', surveyController.show);
 routes.post('/surveys', surveyController.create);
+routes.post('/sendMail', sendMailController.execute);
 
 export { routes };
