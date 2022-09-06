4c04a174990a3500d012f4c9246a5d14a00a757e
Rayan Wilbert <rayan.rmw@gmail.com>
2022-09-05 23:45:48 -0300

    Handling Exceptions and Errors :monocle_face:

--- src/controllers/AnswerController.ts
+++ src/controllers/AnswerController.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from 'express';
 import { getCustomRepository } from 'typeorm';
 import { SurveysUsersRepository } from '../repositories/SurveysUsersRepository';
+import { AppError } from '../errors/AppError';
 
 class AnswerController {
   async execute(request: Request, response: Response) {
@@ -8,10 +9,7 @@ class AnswerController {
     const { u: id } = request.query;
     const surveysUsersRepository = getCustomRepository(SurveysUsersRepository);
     const surveyUser = await surveysUsersRepository.findOne(String(id));
-    if (!surveyUser)
-      return response
-        .status(400)
-        .json({ error: 'Survey User does not exists!' });
+    if (!surveyUser) throw new AppError('Survey User does not exists!');
     surveyUser.value = Number(value);
     await surveysUsersRepository.save(surveyUser);
     return response.json(surveyUser);
--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -2,6 +2,7 @@ import { Request, Response } from 'express';
 import { getCustomRepository } from 'typeorm';
 import { UsersRepository } from '../repositories/UsersRepository';
 import * as yup from 'yup';
+import { AppError } from '../errors/AppError';
 
 class UserController {
   async create(request: Request, response: Response) {
@@ -15,13 +16,12 @@ class UserController {
     try {
       await schema.validate(request.body, { abortEarly: false });
     } catch (error) {
-      return response.status(400).json({ error });
+      throw new AppError(error);
     }
 
     const usersRepository = getCustomRepository(UsersRepository);
     const userAlreadyExists = await usersRepository.findOne({ email });
-    if (userAlreadyExists)
-      return response.status(400).json({ error: 'User already exists!' });
+    if (userAlreadyExists) throw new AppError('User already exists!')
     const user = usersRepository.create({ name, email });
     await usersRepository.save(user);
     return response.status(201).json(user);
