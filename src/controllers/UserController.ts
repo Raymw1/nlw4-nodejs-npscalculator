d38fe2e57d210be79a050cd7b4023463e5d7d0c7
Rayan Wilbert <rayan.rmw@gmail.com>
2022-09-05 22:53:42 -0300

    User fields validation done

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,10 +1,23 @@
 import { Request, Response } from 'express';
 import { getCustomRepository } from 'typeorm';
 import { UsersRepository } from '../repositories/UsersRepository';
+import * as yup from 'yup';
 
 class UserController {
   async create(request: Request, response: Response) {
     const { name, email } = request.body;
+    const schema = yup.object().shape({
+      name: yup.string().required(),
+      email: yup.string().email().required(),
+    });
+    /* if (!(await schema.isValid(request.body))) */
+    /*   return response.status(400).json({ error: 'Validation failed!' }); */
+    try {
+      await schema.validate(request.body, { abortEarly: false });
+    } catch (error) {
+      return response.status(400).json({ error });
+    }
+
     const usersRepository = getCustomRepository(UsersRepository);
     const userAlreadyExists = await usersRepository.findOne({ email });
     if (userAlreadyExists)
